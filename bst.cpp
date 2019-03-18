4e34f770b6632ae356334a6b8773a75a923c7fb2
LukasHain <lukashain9@gmail.com>
2019-03-18 19:06:06 +0100

    update

--- bst.cpp
+++ bst.cpp
@@ -10,3 +10,173 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "bst.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "general.h"
+
+
+struct Node {
+  int data;
+  Bst left;
+  Bst right;
+};
+
+
+/**
+*** Initializes the binary search tree. Deletes all existing nodes in the tree
+*** @return The number of deleted nodes
+*/
+Bst new_bst()
+{
+  Bst node = (Bst)malloc(sizeof(struct Node));
+  node = 0;
+  return node;
+}
+
+void delete_bst(Bst bst)
+{
+  if(bst == 0)
+    {
+      return;
+    }
+    delete_bst(bst->left);
+    delete_bst(bst->right);
+    sfree(bst);
+}
+
+/**
+*** @return The depth of the BST
+*/
+int get_depth(Bst bst)
+{
+  if(bst == 0)
+  {
+    return 0;
+  }
+
+  int l = get_depth(bst->left)+1;
+  int r = get_depth(bst->right)+1;
+  if (r > l) {
+    return r;
+  }
+  return l;
+}
+
+/**
+*** Adds a value to the BST
+*/
+void add(Bst* bst, int value)
+{
+  Bst &bst1 = *bst;
+
+  if (bst1 == 0) {
+    Bst node = (Bst)malloc(sizeof(struct Node));
+    node->data = value;
+    node->left = 0;
+    node->right = 0;
+    bst1 = node;
+  }
+  else if (value <= bst1->data) {
+    add(&bst1->left, value);
+  }
+  else{
+    add(&bst1->right, value);
+  }
+}
+
+/**
+*** @return The value of the root element of the BST
+*/
+int root_value(Bst bst)
+{
+  return bst->data;
+}
+
+/**
+*** @return The left subtree of the BST
+*/
+Bst left_subtree(Bst root)
+{
+  return root->left;
+}
+
+/**
+*** @return The right subtree of the BST
+*/
+Bst right_subtree(Bst root)
+{
+  return root->right;
+}
+
+/**
+*** Traverses the BST depth first by first returning the root, then traversing the left
+*** subtree, then traversing the right subtree.
+*** @param bst The BST to be traversed
+*** @param elements Array of elements in the order how they are found during traversal
+*** @param start Start index of elements wherer the function should start to add the found elements
+*** @return Number of elements found during traversal
+*/
+int traverse_pre_order(Bst bst, int *elements, int start)
+{
+  return 0;
+}
+
+/**
+*** Traverses the BST depth first by first traversing the left subtree, then adding the root,
+*** then traversing the right subtree.
+*** @param bst The BST to be traversed
+*** @param elements Array of elements in the order how they are found during traversal
+*** @param start Start index of elements wherer the function should start to add the found elements
+*** @return Number of elements found during traversal
+*/
+int traverse_in_order(Bst bst, int *elements, int start)
+{
+  return 0;
+}
+
+/**
+*** Traverses the BST depth first by first traversing the left subtree, then traversing the right
+*** subtree and finally adding the root.
+*** @param bst The BST to be traversed
+*** @param elements Array of elements in the order how they are found during traversal
+*** @param start Start index of elements wherer the function should start to add the found elements
+*** @return Number of elements found during traversal
+*/
+int traverse_post_order(Bst bst, int *elements, int start)
+{
+  return 0;
+}
+
+/**
+*** Checks whether two trees are equal
+*** @param bst1 First bst
+*** @param bst2 Second bst
+*** @return true if bst1 and bst2 are equal, false otherwise
+*/
+bool are_equal(Bst bst1, Bst bst2)
+{
+  return 0;
+}
+
+/**
+*** Calculates the longest branch in the tree. If two branches are equally long
+*** the left branch is taken.
+*** @param bst Bst which longest branch should be found
+*** @return A tree where each node has only one child equal to the longest
+*** branch of bst
+*/
+void most_left_longest_branch(Bst bst, Bst* branch)
+{
+
+}
+
+/**
+*** Gets the number of non-empty subtrees of a tree
+*** @param bst The tree which number of subtrees to get
+*** @return The number of non-empty subtrees
+*/
+int get_number_of_subtrees(Bst bst)
+{
+  return 0;
+}
